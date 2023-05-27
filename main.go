1141236be4104e8fd66bee26699707a0fe82580c
Sajal Saraf <sajalsaraf161203@gmail.com>
2023-05-27 13:20:31 +0530

    Database

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	db, err := gorm.Open(mysql.Open("root:Sajal@123@/adminapp"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to connect database")
+	}
+	fmt.Println(db)
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("Hello, Sajal 👋!")
 	})
 
 	app.Listen(":8000")
