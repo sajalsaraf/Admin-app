cbd62714c6f69bb906d740251a0c2bef9c6adcb6
Sajal Saraf <sajalsaraf161203@gmail.com>
2023-06-04 10:45:32 +0530

    ForeignKey

--- controllers/authController.go
+++ controllers/authController.go
@@ -47,6 +47,7 @@ func Register(c *fiber.Ctx) error {
 	user.Firstname = data["firstname"]
 	user.Lastname = data["lastname"]
 	user.Email = data["email"]
+	user.RoleId = 1
 	// user.Password = password
 	user.SetPassword(pwd_string)
 
