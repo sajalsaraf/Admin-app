58cf196def6570b68dd40a2f7859f7b4c4f85a9e
Sajal Saraf <sajalsaraf161203@gmail.com>
2023-06-04 13:24:39 +0530

    Manytomanyrealtions

--- controllers/roleController.go
+++ controllers/roleController.go
@@ -11,18 +11,37 @@ import (
 func AllRoles(c *fiber.Ctx) error {
 	var roles []models.Role
 
-	database.DB.Find(&roles)
+	database.DB.Preload("Permissions").Find(&roles)
 
 	return c.JSON(roles)
 }
 
-func CreateRole(c *fiber.Ctx) error {
-	var role models.Role
+type RoleCreateDTO struct {
+	name        string
+	permissions []string
+}
 
-	if err := c.BodyParser(&role); err != nil {
+func CreateRole(c *fiber.Ctx) error {
+	var roleDto fiber.Map
+
+	if err := c.BodyParser(&roleDto); err != nil {
 		return err
 	}
+	list := roleDto["permissions"].([]interface{})
 
+	permissions := make([]models.Permission, len(list))
+
+	for i, permissionId := range list {
+		id, _ := strconv.Atoi(permissionId.(string))
+		permissions[i] = models.Permission{
+			Id: uint(id),
+		}
+	}
+
+	role := models.Role{
+		Name:        roleDto["name"].(string),
+		Permissions: permissions,
+	}
 	database.DB.Create(&role)
 
 	return c.JSON(role)
@@ -34,20 +53,38 @@ func GetRole(c *fiber.Ctx) error {
 	role := models.Role{
 		Id: uint(id),
 	}
-	database.DB.Find(&role)
+	database.DB.Preload("Permissions").Find(&role)
 	return c.JSON(role)
 }
 
 func UpdateRole(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
+	var roleDto fiber.Map
 
-	role := models.Role{
-		Id: uint(id),
-	}
-
-	if err := c.BodyParser(&role); err != nil {
+	if err := c.BodyParser(&roleDto); err != nil {
 		return err
 	}
+	list := roleDto["permissions"].([]interface{})
+
+	permissions := make([]models.Permission, len(list))
+
+	for i, permissionId := range list {
+		id, _ := strconv.Atoi(permissionId.(string))
+		permissions[i] = models.Permission{
+			Id: uint(id),
+		}
+	}
+	// var result interface{}
+
+	// database.DB.Table("role_permissions").Where("role_id = ?", uint(id)).Delete(&result)
+	database.DB.Exec("DELETE FROM role_permissions where role_id = ?", id)
+
+	role := models.Role{
+		Id:          uint(id),
+		Name:        roleDto["name"].(string),
+		Permissions: permissions,
+	}
+
 	database.DB.Model(&role).Updates(role)
 	return c.JSON(role)
 }
