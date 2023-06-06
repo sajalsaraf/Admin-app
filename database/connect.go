d6fd204a910cec3360bf5c3b5b663cc27df52a63
Sajal Saraf <sajalsaraf161203@gmail.com>
2023-06-06 15:27:04 +0530

    Products

--- database/connect.go
+++ database/connect.go
@@ -18,5 +18,5 @@ func Connect() {
 	fmt.Println("sajal")
 	DB = database
 
-	database.AutoMigrate(&models.User{}, &models.Role{}, &models.Permission{})
+	database.AutoMigrate(&models.User{}, &models.Role{}, &models.Permission{}, &models.Product{})
 }
